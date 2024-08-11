5c86071b80388ec348252837d97666e8f99ce2bb
sebastian_iwasieczko@web.de <you@example.com>
2024-08-11 18:00:31 +0200

    pushing tasks into database

--- addTask/addTask.js
+++ addTask/addTask.js
@@ -1,6 +1,50 @@
-const BASE_URL = "https://console.firebase.google.com/project/join-projekt-f44bb/database/join-projekt-f44bb-default-rtdb/data/~2F";
+const BASE_URL = "https://join-projekt-f44bb-default-rtdb.europe-west1.firebasedatabase.app/";
 
-async function includeHTML() {
+function createTask() {
+    let task = {
+        title: document.getElementById("title-input").value,
+        storyDescrip: document.getElementById("textarea-input").value,
+        name: document.getElementById("selected-name").value,
+        date: document.getElementById("date-input").value,
+        urgency: selectedPrio,
+        subtask: document.getElementById("choose-subtasks").value,
+    };
+
+    addTask("/tasks", task);
+}
+
+async function addTask(path, data) {
+    let response = await fetch(BASE_URL + path + ".json", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    if (response.ok) {
+        let responseToJson = await response.json();
+        console.log(responseToJson);
+    } else {
+        console.error("Error adding task:", response.statusText);
+    }
+}
+
+function fetchContacts() {}
+
+function choosePrio(prio) {
+    document.getElementById("prio-high-button").style.background = "";
+    document.getElementById("prio-medium-button").style.background = "";
+    document.getElementById("prio-low-button").style.background = "";
+
+    document.getElementById(`prio-${prio}-button`).style.background = "red";
+    selectedPrio = prio;
+}
+
+/**
+ * 
+ * 
+ * 
+ * async function includeHTML() {
     let includeElements = document.querySelectorAll("[includeHTML]");
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
@@ -13,27 +57,16 @@ async function includeHTML() {
         }
     }
 }
-
-async function addTask(path = "", data = {}) {
-    let response = await fetch(BASE_URL + ".json", {
-        method: "POST",
-        header: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    });
-    return (responseToJson = await response.json());
-}
-
-function choosePrio(prio) {
-    document.getElementById("prio-urgent-button").classList.remove("prio-urgent-button-bg-color");
-    document.getElementById("prio-medium-button").classList.remove("prio-medium-button-bg-color");
-    document.getElementById("prio-low-button").classList.remove("prio-low-button-bg-color");
-    selectedPrio = prio;
-    title: document.getElementById("title-input").value;
-    storyDescrip: document.getElementById("textarea-input").value;
-    name: document.getElementById("selected-name").value;
-    date: document.getElementById("date-input").value;
-    urgency: selectedPrio;
-    subtask: document.getElementById("choose-subtasks").value;
-}
+ * 
+function submitTask() {
+    let data = {
+        title: document.getElementById("title-input").value,
+        storyDescrip: document.getElementById("textarea-input").value,
+        name: document.getElementById("selected-name").value,
+        date: document.getElementById("date-input").value,
+        urgency: selectedPrio,
+        subtask: document.getElementById("choose-subtasks").value,
+    };
+    console.log(data);
+} 
+ */
