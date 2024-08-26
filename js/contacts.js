fb3341afc6bfb8e8186e90060c134570caef23ea
Susanne714 <sambale.su@gmail.com>
2024-08-26 15:16:06 +0200

    Firebase link updated

--- js/contacts.js
+++ js/contacts.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://joindev-9be03-default-rtdb.europe-west1.firebasedatabase.app/';
+const BASE_URL = 'https://join285-60782-default-rtdb.europe-west1.firebasedatabase.app/';
 
 const addContactForm = document.getElementById('addContactForm');
 let contactsArray = [];
