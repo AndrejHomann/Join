57e70256504c9c1b6ba88396023a5ddc902bbe56
Susanne714 <sambale.su@gmail.com>
2024-10-29 13:34:52 +0100

    added prio functions and prio array

--- boardTest/config.js
+++ boardTest/config.js
@@ -1,3 +1,29 @@
 const BASE_URL = 'https://join285-60782-default-rtdb.europe-west1.firebasedatabase.app';
 
-globalContactList = [];
+let selectedPrio = "medium";
+
+function choosePrio(prio) {
+    let selectedPioButton = document.getElementById(`prio-${prio}-button`);
+
+    if (selectedPrio === prio) {
+        resetPrio();
+        selectedPrio = "";
+    } else {
+        resetPrio();
+
+        selectedPioButton.classList.add(`prio-${prio}-button-bg-color`);
+        selectedPioButton.classList.remove("prio-default-text-color");
+
+        selectedPrio = prio;
+    }
+}
+
+function resetPrio() {
+    document.getElementById("prio-urgent-button").classList.remove("prio-urgent-button-bg-color");
+    document.getElementById("prio-medium-button").classList.remove("prio-medium-button-bg-color");
+    document.getElementById("prio-low-button").classList.remove("prio-low-button-bg-color");
+
+    document.getElementById("prio-urgent-button").classList.add("prio-default-text-color");
+    document.getElementById("prio-medium-button").classList.add("prio-default-text-color");
+    document.getElementById("prio-low-button").classList.add("prio-default-text-color");
+}
